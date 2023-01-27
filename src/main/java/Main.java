d85d41329a9e7a8e53b9826f5b6c238c19e786f1
Gabagooooooooooool <116589326+Gabagooooooooooool@users.noreply.github.com>
2023-01-27 22:23:24 +0100

    fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,23 +8,23 @@ import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        // Wait for game to turn off (5 seconds is enough I think)
-        Thread.sleep(5000);
+        // Wait
+        Thread.sleep(Integer.parseInt(args[0]));
         // Delete old mod file
-        File modFile = new File(args[0]);
+        File modFile = new File(args[1]);
         if (!modFile.delete()) {
             JOptionPane.showMessageDialog(null, "Update failed! Couldn't delete the file provided.");
             return;
         }
         // Download mod
-        switch(args[1]){
+        switch(args[2]){
             // Main repository download
             case "mainrepo":
                 FileUtils.copyURLToFile(new URL("https://github.com/OctoSplash01/AuroraClient/releases/latest/download/bin.jar"), modFile);
                 break;
             // Custom repository download
             case "customrepo":
-                FileUtils.copyURLToFile(new URL(args[2]), modFile);
+                FileUtils.copyURLToFile(new URL(args[3]), modFile);
                 break;
         }
         // Status notify
