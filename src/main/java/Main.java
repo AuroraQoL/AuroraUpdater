30d16ce5d1205133b011fb3fafc7c39152a32172
Gaba <116589326+Gabagooooooooooool@users.noreply.github.com>
2023-04-06 17:57:53 +0200

    Reformat & Shutdown Fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,26 +8,12 @@ import java.net.URL;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        // Wait
         Thread.sleep(Integer.parseInt(args[0]));
-        // Delete old mod file
         File modFile = new File(args[1]);
         if (!modFile.delete()) {
             JOptionPane.showMessageDialog(null, "Update failed! Couldn't delete the file provided.");
-            return;
+        } else {
+            FileUtils.copyURLToFile(new URL(args[2].equals("customrepo") ? args[3] : "https://github.com/AuroraQoL/AuroraClient/releases/latest/download/bin.jar"), modFile);
         }
-        // Download mod
-        switch(args[2]){
-            // Main repository download
-            case "mainrepo":
-                FileUtils.copyURLToFile(new URL("https://github.com/OctoSplash01/AuroraClient/releases/latest/download/bin.jar"), modFile);
-                break;
-            // Custom repository download
-            case "customrepo":
-                FileUtils.copyURLToFile(new URL(args[3]), modFile);
-                break;
-        }
-        // Status notify
-     //   JOptionPane.showMessageDialog(null, "Updated Aurora!");
     }
 }
